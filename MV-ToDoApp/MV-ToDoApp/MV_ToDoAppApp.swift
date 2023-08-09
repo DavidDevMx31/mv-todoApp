3da73bbc11480af69404a3f410f34f864d48e2dc
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2023-08-09 17:48:08 -0600

    Commit inicial

---
+++ MV-ToDoApp/MV-ToDoApp/MV_ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MV_ToDoAppApp.swift
+//  MV-ToDoApp
+//
+//  Created by David Ali on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MV_ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
