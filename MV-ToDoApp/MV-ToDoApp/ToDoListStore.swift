b536e684e9597bc154fda1f012dda287fe208421
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2023-08-09 21:43:02 -0600

    Agrega store a pantalla ToDo List

--- MV-ToDoApp/MV-ToDoApp/ToDoListStore.swift
+++ MV-ToDoApp/MV-ToDoApp/ToDoListStore.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+@MainActor
+class ToDoListStore: ObservableObject {
+    //MARK: Properties
+    private let listApi: ToDoListApi
+    
+    //MARK: Published properties
+    @Published var list: ToDoList
+    
+    //MARK: Init
+    init(listSource: ToDoListApi) {
+        listApi = listSource
+        list = ToDoList(name: "Nueva lista", items: [])
+    }
+    
+    func getListItems() async {
+        list = ToDoList(name: "Mi lista", items: await listApi.fetchToDoItems())
+    }
+}
