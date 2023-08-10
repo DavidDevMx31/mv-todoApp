ce84ef288aa635366bce8ec6b9ca54cbe8518b10
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2023-08-09 21:44:55 -0600

    Cambia estatus de items

--- MV-ToDoApp/MV-ToDoApp/Services/ToDoList/ToDoList_LocalService.swift
+++ MV-ToDoApp/MV-ToDoApp/Services/ToDoList/ToDoList_LocalService.swift
@@ -12,7 +12,7 @@ class ToDoList_LocalService: ToDoListApi {
     func fetchToDoItems() async -> [ToDoItem] {
         return [
             ToDoItem(title: "Tarea 1", description: "Esta es la tarea 1"),
-            ToDoItem(title: "Tarea 2", description: "Esta es la tarea 2"),
+            ToDoItem(title: "Tarea 2", description: "Esta es la tarea 2", isCompleted: true),
             ToDoItem(title: "Tarea 3", description: "Esta es la tarea 3"),
             ToDoItem(title: "Tarea 4", description: "Esta es la tarea 4"),
             ToDoItem(title: "Tarea 5", description: "Esta es la tarea 5")
