339ff02a4e22347a331ee876dc1d012afb8e8191
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2023-08-09 21:43:29 -0600

    Agrega disenio de pantalla ToDo List

--- MV-ToDoApp/MV-ToDoApp/Views/ToDoListView.swift
+++ MV-ToDoApp/MV-ToDoApp/Views/ToDoListView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    @StateObject var store: ToDoListStore = ToDoListStore(listSource: ToDoList_LocalService())
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            if store.list.items.count != 0 {
+                List(store.list.items) { item in
+                    TodoItemCell(item: item)
+                }
+                .navigationTitle(store.list.name)
+            } else {
+                EmptyListView()
+            }
+        }
+        .task {
+            await store.getListItems()
+        }
+    }
+}
+
+struct TodoItemCell: View {
+    let item: ToDoItem
+    
+    var body: some View {
+        HStack {
+            if item.isCompleted {
+                Image(systemName: "checkmark.circle.fill")
+                    .foregroundColor(.green)
+            }
+            Text(item.title)
+        }
+    }
+}
+
+struct EmptyListView: View {
+    var body: some View {
+        Text("La lista está vacía.")
     }
 }
 
